324b1d29e50505b28ae9f4ad418267b87dc8e8c0
sm-jahangir <samraatjahangir@gmail.com>
2022-01-27 02:07:36 +0600

    Progressbar added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueProgressBar from 'vue-progressbar'
 import App from './App.vue'
 
 Vue.config.productionTip = false
@@ -7,6 +8,23 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 // Router
 import router from "./router";
+
+// For Vue Progressbar
+const vueProgressbarOptions = {
+  color: '#bffaf3',
+  failedColor: '#874b4b',
+  thickness: '5px',
+  transition: {
+    speed: '0.2s',
+    opacity: '0.6s',
+    termination: 300
+  },
+  autoRevert: true,
+  location: 'left',
+  inverse: false
+}
+
+Vue.use(VueProgressBar, vueProgressbarOptions)
 
 new Vue({
   render: h => h(App),

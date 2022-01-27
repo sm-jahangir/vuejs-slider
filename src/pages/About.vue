65c1887b08fba5f4a6cca5b01a33ecdbbe1801dc
sm-jahangir <samraatjahangir@gmail.com>
2022-01-27 10:02:01 +0600

    Owl carousel completed

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,13 +1,50 @@
 <template lang="">
-    <div>
+    <div class="container">
         <h1>This is About Page </h1>
+        <hr />
+        <carousel :autoplay="true" :nav="false" :dots="false" :responsive="{0:{items:1},600:{items:1}}">
+            <template slot="prev"><span class="prev cursor-pointer">prev</span></template>
+                <div class="box bg-warning p-4 mx-1 rounded fw-bolder">
+                    <img src="https://via.placeholder.com/350x150" class="img-fluid" alt="">
+                    <p>Your Content 1  </p>
+                </div>
+                <div class="box bg-warning p-4 mx-1 rounded fw-bolder">
+                    <img src="https://via.placeholder.com/350x150" class="img-fluid" alt="">
+                    <p>Your Content 2  </p>
+                </div>
+                <div class="box bg-warning p-4 mx-1 rounded fw-bolder">
+                    <img src="https://via.placeholder.com/350x150" class="img-fluid" alt="">
+                    <p>Your Content 3  </p>
+                </div>
+                <div class="box bg-warning p-4 mx-1 rounded fw-bolder">
+                    <img src="https://via.placeholder.com/350x150" class="img-fluid" alt="">
+                    <p>Your Content 4  </p>
+                </div>
+                <div class="box bg-warning p-4 mx-1 rounded fw-bolder">
+                    <img src="https://via.placeholder.com/350x150" class="img-fluid" alt="">
+                    <p>Your Content 5  </p>
+                </div>
+                <div class="box bg-warning p-4 mx-1 rounded fw-bolder">
+                    <img src="https://via.placeholder.com/350x150" class="img-fluid" alt="">
+                    <p>Your Content 6  </p>
+                </div>
+            <template slot="next"><span class="next cursor-pointer">next</span></template>
+        </carousel>
     </div>
 </template>
 <script>
+
+import carousel from 'vue-owl-carousel'
+
 export default {
-    
+    components: { carousel },
 }
+
 </script>
-<style lang="">
-    
+
+
+<style lang="css">
+    .cursor-pointer {
+        cursor: pointer;
+    }
 </style>
